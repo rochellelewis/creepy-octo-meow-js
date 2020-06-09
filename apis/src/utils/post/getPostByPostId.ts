3acb7209fcbd7606cb31c0d60fe4c709982f5947
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-09 17:26:10 -0600

    add getPostByPostId.ts mysql enabled function

---
+++ apis/src/utils/post/getPostByPostId.ts
@@ -0,0 +1,20 @@
+import {Post} from "../interfaces/post";
+import {connect} from "../../database";
+
+async function getPostByPostId(postId : string) {
+	try {
+
+		const mysqlConnection = await connect();
+
+		// mysql prepared statement
+		const mySqlQuery = 'SELECT BIN_TO_UUID(postId) as postId, BIN_TO_UUID(postProfileId) as postProfileId, postContent, postDate, postTitle FROM post WHERE postId = UUID_TO_BIN(:postId)'
+
+		// return the rows from DB
+		const [rows] =  await mysqlConnection.execute(mySqlQuery, postId)
+		return rows
+
+	} catch(error) {
+		console.log(error)
+		return undefined
+	}
+}
