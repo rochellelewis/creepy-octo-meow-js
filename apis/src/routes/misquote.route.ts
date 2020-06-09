f0181f6efed5d34b5a25821ee7c2ba6d48112460
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-09 00:33:00 -0600

    add boilerplate misquote routes and controllers

---
+++ apis/src/routes/misquote.route.ts
@@ -0,0 +1,9 @@
+
+import {Router} from "express";
+import {getAllMisquoteController, insertMisquoteController} from "../controllers/misquote.contoller";
+
+export const MisquoteRoute = Router()
+
+MisquoteRoute.route("/")
+	.get(getAllMisquoteController)
+	.post(insertMisquoteController)
