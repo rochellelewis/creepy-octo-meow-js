dbee2dc79d0c75f05140345ee30a4e939044aea5
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-09 14:50:03 -0600

    add boilerplate misquote sample interface, status interface

---
+++ apis/src/interfaces/misquote.ts
@@ -0,0 +1,7 @@
+/* interfaces define the structure of the object */
+export interface Misquote {
+	misquoteId: string | null,
+	misquoteAttribution: string,
+	misquoteContent: string,
+	misquoteSubmitter: string
+}
