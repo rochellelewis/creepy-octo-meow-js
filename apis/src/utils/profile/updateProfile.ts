7eecf4fc6fce90d55a121637c7f2f22dc0ca21d9
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-09 16:38:58 -0600

    add updateProfile mysql enabled function

---
+++ apis/src/utils/profile/updateProfile.ts
@@ -0,0 +1,17 @@
+import {Profile} from "../interfaces/profile";
+import {connect} from "../../database";
+
+export async function updateProfile(profile: Profile) {
+	try {
+
+		const mySqlConnection = await connect();
+
+		const mySqlQuery = "UPDATE profile SET profileActivationToken = :profileActivationToken, profileEmail = :profileEmail, profileHash = :profileHash, profileUsername = :profileUsername WHERE profileId = UUID_TO_BIN(:profileId)";
+
+		const [rows] = await mySqlConnection.execute(mySqlQuery, profile)
+		return "Profile updated successfully! :D"
+
+	} catch(error) {
+		console.log(error)
+	}
+}
