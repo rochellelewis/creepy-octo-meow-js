e1d2c97d93f3602e11061b8aea44b71bcee986c6
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-10 03:02:22 -0600

    add dockblock, update proper parameter in updatePost.ts

--- apis/src/utils/post/updatePost.ts
+++ apis/src/utils/post/updatePost.ts
@@ -1,14 +1,19 @@
 import {Post} from "../interfaces/post";
 import {connect} from "../../database";
 
-export async function updatePost(post: Post) {
+/**
+ * Updates an existing post
+ *
+ * @param {string} postId - id of the post to be updated
+ **/
+export async function updatePost(postId: string) {
 	try {
 
 		const mySqlConnection = await connect();
 
 		const mySqlQuery = "UPDATE post SET postProfileId = UUID_TO_BIN(:postProfileId), postContent = :postContent, postDate = :postDate, postTitle = :postTitle WHERE postId = UUID_TO_BIN(:postId)";
 
-		const [rows] = await mySqlConnection.execute(mySqlQuery, post)
+		const [rows] = await mySqlConnection.execute(mySqlQuery, postId)
 		return "Meow updated successfully! :D"
 
 	} catch(error) {
