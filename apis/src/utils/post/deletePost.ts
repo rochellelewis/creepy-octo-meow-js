8725f55545bfbdebab2dfaf920067fd5d586a321
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-10 02:57:16 -0600

    update parameter in deletePost.ts

--- apis/src/utils/post/deletePost.ts
+++ apis/src/utils/post/deletePost.ts
@@ -1,14 +1,19 @@
 import {Post} from "../interfaces/post";
 import {connect} from "../../database";
 
-export async function deletePost(post: Post) {
+/**
+ * Deletes a post from mySQL
+ *
+ * @param {string} postId - id of the post to be deleted
+ **/
+export async function deletePost(postId: string) {
 	try {
 
 		const mySqlConnection = await connect();
 
 		const mySqlQuery = "DELETE FROM post WHERE postId = UUID_TO_BIN(:postId)";
 
-		const [rows] = await mySqlConnection.execute(mySqlQuery, post)
+		const [rows] = await mySqlConnection.execute(mySqlQuery, postId)
 		return "Meow deleted successfully! :("
 
 	} catch(error) {
