dae727480ae7362e6b17d6d9bec35fbaae706b24
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-11 16:49:53 -0600

    add sign-in.validator.ts

---
+++ apis/src/validators/sign-in.validator.ts
@@ -0,0 +1,20 @@
+/**
+ * Validates and sanitizes sign in data from end user
+ **/
+export const signInValidator = {
+	profilePassword: {
+		isLength: {
+			errorMessage: 'Password must be at least eight characters',
+			options: { min: 8 }
+		},
+		trim: true,
+		escape: true
+	},
+	profileEmail: {
+		isEmail: {
+			errorMessage: 'Please provide a valid email'
+		},
+		normalizeEmail: true,
+		trim: true
+	}
+};
