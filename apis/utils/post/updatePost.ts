71e7710a38f5823ecf1521d911fac61e6be473a5
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-10 21:54:28 -0600

    fix updatePost to accept a Post object parameter

--- apis/utils/post/updatePost.ts
+++ apis/utils/post/updatePost.ts
@@ -4,16 +4,16 @@ import {connect} from "../../src/database";
 /**
  * Updates an existing post
  *
- * @param {string} postId - id of the post to be updated
+ * @param {Post} post to be updated
  **/
-export async function updatePost(postId: string) {
+export async function updatePost(post: Post) {
 	try {
 
 		const mySqlConnection = await connect();
 
 		const mySqlQuery = "UPDATE post SET postProfileId = UUID_TO_BIN(:postProfileId), postContent = :postContent, postDate = :postDate, postTitle = :postTitle WHERE postId = UUID_TO_BIN(:postId)";
 
-		const [rows] = await mySqlConnection.execute(mySqlQuery, postId)
+		const [rows] = await mySqlConnection.execute(mySqlQuery, post)
 		return "Meow updated successfully! :D"
 
 	} catch(error) {
