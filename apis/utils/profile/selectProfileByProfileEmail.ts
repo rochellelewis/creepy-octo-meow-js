5e59bcc914e54a8d9ff094cb08e2c009d67b3f72
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-11 16:05:26 -0600

    add selectProfileByProfileEmail.ts

---
+++ apis/utils/profile/selectProfileByProfileEmail.ts
@@ -0,0 +1,25 @@
+import {connect} from "../../src/database";
+
+/**
+ * Gets a profile by profile id
+ *
+ * @param {string} profileEmail - id of the profile to select from mysql
+ * @return {(Array | undefined)} rows - array that contains the profile data found, or undefined if errors occur
+ **/
+export async function selectProfileByProfileEmail(profileEmail : string) {
+	try {
+
+		const mysqlConnection = await connect();
+
+		// mysql prepared statement
+		const mySqlQuery = 'SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileEmail, profileHash, profileUsername FROM profile WHERE profileEmail = :profileEmail'
+
+		// return the rows from DB
+		const [rows] =  await mysqlConnection.execute(mySqlQuery, profileEmail)
+		return rows
+
+	} catch(error) {
+		console.log(error)
+		return undefined
+	}
+}
