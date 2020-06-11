2feabab475c683a445127073930200638d07b4be
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-10 21:22:44 -0600

    add profile routes, requests

--- apis/src/routes/profile.route.ts
+++ apis/src/routes/profile.route.ts
@@ -1,8 +1,20 @@
 import {Router} from "express";
-//import {getAllMisquoteController, insertMisquoteController} from "../controllers/misquote.controller";
+import {
+	getProfileByProfileActivationTokenController,
+	getProfileByProfileIdController,
+	postProfileController,
+	updateProfileController
+} from "../controllers/profile.controller";
 
 export const ProfileRoute = Router()
 
 ProfileRoute.route("/")
-	.get((req, res) => res.json("Is this thing on?"))
-	// .post((req, res) => res.json())
+	// .get((req, res) => res.json("Is this thing on?"))
+	.post(postProfileController)
+
+ProfileRoute.route("/:profileId")
+	.get(getProfileByProfileIdController)
+	.put(updateProfileController)
+
+ProfileRoute.route("/:profileActivationToken")
+	.get(getProfileByProfileActivationTokenController)
