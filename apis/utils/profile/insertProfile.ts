42be9101c4271f6ab4d467039fabe0ca44460059
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-16 14:44:30 -0600

    add insertProfile.ts

---
+++ apis/utils/profile/insertProfile.ts
@@ -0,0 +1,17 @@
+import {connect} from "../../src/database";
+import {Profile} from "../interfaces/profile";
+
+export async function insertProfile(profile: Profile) {
+	try {
+
+		const mysqlConnection = await connect();
+		const query : string = "INSERT INTO profile(profileId, profileActivationToken, profileEmail, profileHash, profileUsername) VALUES(UUID_TO_BIN(UUID()), :profileActivationToken, :profileEmail, :profileHash, :profileUsername)";
+
+		const [rows] = await mysqlConnection.execute(query, {profile});
+		return 'Profile Successfully Created'
+
+	} catch (e) {
+		console.error(e)
+		return null
+	}
+}
