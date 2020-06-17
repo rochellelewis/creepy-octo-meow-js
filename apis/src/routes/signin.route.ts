ca8df60d366774de95b06ffe41e8cca403f41fc0
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-17 16:27:43 -0600

    update signin.route.ts

--- apis/src/routes/signin.route.ts
+++ apis/src/routes/signin.route.ts
@@ -1,13 +1,10 @@
 import {Router} from 'express';
-
 import {asyncValidatorController} from "../controllers/asyncValidator.controller";
 import {signInValidator} from '../validators/signin.validator';
-
-import {signIn} from "../controllers/signin.controller";
-
+import {signInController} from "../controllers/signin.controller";
 const { checkSchema } = require('express-validator');
 
 export const SignInRoute = Router();
 
 SignInRoute.route('/')
-	.post(asyncValidatorController(checkSchema(signInValidator)), signIn);
+	.post(asyncValidatorController(checkSchema(signInValidator)), signInController);
