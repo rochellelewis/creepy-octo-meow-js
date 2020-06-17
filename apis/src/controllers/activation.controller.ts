cbaa525698d05075cb9bd6a10511897230bc324b
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-17 14:39:18 -0600

    add activation.controller.ts

---
+++ apis/src/controllers/activation.controller.ts
@@ -0,0 +1,37 @@
+import {NextFunction, Request, Response} from "express";
+
+import {Profile} from "../../utils/interfaces/profile";
+import {updateProfile} from "../../utils/profile/updateProfile";
+import {selectProfileByProfileActivationToken} from "../../utils/profile/selectProfileByProfileActivationToken";
+
+export async function activationController(request: Request, response: Response, nextFunction: NextFunction) {
+
+	// grab activation token from request params
+	const {activation} = request.params
+
+	try {
+
+		const profile =  await selectProfileByProfileActivationToken(activation)
+
+		const activationFailed = () => response.json({
+			status: 400,
+			data: null,
+			message: "Account activation has failed. Have you already activated this account"
+		});
+
+		const activationSucceeded = async (profile: Profile) => {
+			const updatedProfile = {...profile, profileActivationToken: null}
+			await updateProfile(updatedProfile)
+			return response.json({
+				status: 200,
+				data: null,
+				message: "Account activation was successful"
+			});
+		}
+
+		profile ? await activationSucceeded(profile) : activationFailed()
+
+	} catch (error) {
+		return response.json({status: 500, data: null, message: error.message})
+	}
+}
