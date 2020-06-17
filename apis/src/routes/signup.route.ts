cd91bc190a124e7b3511f1f9a785d69edaaf8579
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-17 16:04:13 -0600

    update signup.route.ts with activation route

--- apis/src/routes/signup.route.ts
+++ apis/src/routes/signup.route.ts
@@ -1,8 +1,10 @@
 import {Router} from "express";
+import {check, param} from "express-validator";
 
 import {signUpProfileController} from '../controllers/signup.controller';
-
+import {activationController} from "../controllers/activation.controller";
 import {asyncValidatorController} from "../controllers/asyncValidator.controller";
+
 import {signUpValidator} from '../validators/signup.validator';
 
 const { checkSchema } = require('express-validator');
@@ -11,3 +13,6 @@ export const SignUpRoute = Router()
 
 SignUpRoute.route("/")
 	.post(asyncValidatorController(checkSchema(signUpValidator)), signUpProfileController);
+
+SignUpRoute.route("/activation/:activationToken")
+	.get(asyncValidatorController(param("activation", "invalid activation link").isHexadecimal().notEmpty()), activationController)
