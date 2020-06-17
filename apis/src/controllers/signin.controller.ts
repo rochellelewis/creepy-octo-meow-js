ec03dfdcf57f28f0a9ac8e213b7641bab06dc5c6
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-17 16:26:05 -0600

    update signin.controller.ts boilerplate

--- apis/src/controllers/signin.controller.ts
+++ apis/src/controllers/signin.controller.ts
@@ -1,25 +1,24 @@
 import {NextFunction, Response, Request} from 'express';
 import  "express-session";
+import passport from "passport";
+import passportLocal, {Strategy} from 'passport-local';
+
 import uuid from "uuid";
-
 import {generateJwt, validatePassword} from "../../utils/auth.utils";
-import {Profile} from "../../utils/interfaces/profile";
 
-const passport = require("passport");
-const {validationResult} = require('express-validator');
+import {Profile} from "../../utils/interfaces/profile";
+import {selectProfileByProfileEmail} from "../../utils/profile/selectProfileByProfileEmail";
 
 /**
- * Handles POST request for user authentication
+ * Handles POST request for sign in user authentication
  *
  * @param request
  * @param response
  * @param nextFunction
  **/
-export async function signIn(request: Request, response: Response, nextFunction: NextFunction) {
+export async function signInController(request: Request, response: Response, nextFunction: NextFunction) {
 
 	try {
-
-		validationResult(request).throw();
 
 		// grab the profile password from the request body
 		const {profilePassword} = request.body;
@@ -30,10 +29,14 @@ export async function signIn(request: Request, response: Response, nextFunction:
 			async (err: any, passportUser: Profile) => {
 
 				const {profileId, profileEmail} = passportUser;
-
 				const signature : string = uuid();
-
 				const authorization : string = generateJwt({profileId, profileEmail}, signature);
+
+				const signInFailed = (message: string) => response.json({
+					status: 400,
+					data: null,
+					message
+				});
 
 				const signInSuccessful = () => {
 
@@ -47,17 +50,11 @@ export async function signIn(request: Request, response: Response, nextFunction:
 						authorization
 					});
 
-					return response.json({status: 200, data: null, message: "sign in successful"})
+					return response.json({status: 200, data: null, message: "Sign in successful!"})
 				};
 
-				const signInFailed = () => response.json({
-					status: 400,
-					data: null,
-					message: "incorrect username or password"
-				});
-
 				const isPasswordValid: boolean = passportUser && await validatePassword(passportUser.profileHash, profilePassword);
-				return isPasswordValid ? signInSuccessful() : signInFailed();
+				return isPasswordValid ? signInSuccessful() : signInFailed("Email or password invalid.");
 
 			})(request, response, nextFunction)
 
@@ -65,3 +62,23 @@ export async function signIn(request: Request, response: Response, nextFunction:
 		return response.json({status: 500, data: null, message: error.message})
 	}
 }
+
+const LocalStrategy = passportLocal.Strategy;
+
+const passportStrategy : Strategy = new LocalStrategy(
+	{
+		usernameField: 'profileEmail',
+		passwordField: "profilePassword"
+	},
+	async (email, password, done) => {
+		try {
+
+			const profile : Profile | undefined = await selectProfileByProfileEmail(email);
+			return profile ? done(null, profile) : done(undefined, undefined, { message: 'Incorrect username or password'});
+
+		} catch (error) {
+			return done(error);
+		}
+	});
+
+export const passportMiddleware = passport.use(passportStrategy);
