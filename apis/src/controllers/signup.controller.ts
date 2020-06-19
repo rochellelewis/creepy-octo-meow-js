109ad9df020342a415466270afb635a6f3d889dd
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-19 02:11:30 -0600

    signup.controller.ts working :raised_hands:

--- apis/src/controllers/signup.controller.ts
+++ apis/src/controllers/signup.controller.ts
@@ -6,7 +6,8 @@ import { Status } from '../../utils/interfaces/status';
 import { setActivationToken, setHash } from '../../utils/auth.utils';
 import { insertProfile } from "../../utils/profile/insertProfile";
 
-const { validationResult } = require('express-validator');
+import MailComposer from "nodemailer/lib/mail-composer";
+const mailgun = require("mailgun-js");
 
 /**
  * Handles POST request for new user Profile insertion
@@ -16,8 +17,6 @@ const { validationResult } = require('express-validator');
  **/
 export async function signUpProfileController (request: Request, response: Response) {
 	try {
-
-		validationResult(request).throw();
 
 		// grab the profile data off of the request body
 		const {
@@ -30,6 +29,23 @@ export async function signUpProfileController (request: Request, response: Respo
 		const profileHash = await setHash(profilePassword);
 		const profileActivationToken = setActivationToken();
 
+		// set base path for account activation link
+		const basePath = `${request.protocol}://${request.get('host')}${request.originalUrl}/activation/${profileActivationToken}`;
+
+		// create a formatted message for the activation email
+		const message = `<h2>Welcome to Creepy Octo Meow v8.0!</h2>
+<p>In order to start posting meows of cats you must confirm your account here:</p>
+<p><a href="${basePath}">${basePath}</a></p>`
+
+		// create mailgun message
+		const mailgunMessage = {
+			from: `Mailgun Sandbox <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+			to: profileEmail,
+			subject: "One step closer to Sticky Head -- Account Activation",
+			text: 'Test email text',
+			html: message
+		}
+
 		// create Profile object to be inserted
 		const profile : Profile = {
 			profileId: null,
@@ -39,9 +55,35 @@ export async function signUpProfileController (request: Request, response: Respo
 			profileUsername
 		};
 
+		// insert profile into mysql
 		const result = await insertProfile(profile);
 
-		return response.json({status: 200, data: null, message: result})
+		// create new mailgun message
+		const emailComposer: MailComposer = new MailComposer(mailgunMessage)
+		emailComposer.compile().build((error: any, message: Buffer) => {
+			const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
+
+			console.log(message.toString("ascii"))
+
+			const compiledEmail = {
+				to: profileEmail,
+				message: message.toString("ascii")
+			}
+
+			const status: Status = {
+				status: 200,
+				message: "Profile successfully created! Please check your email to activate your account.",
+				data: null
+			};
+
+			mg.messages().sendMime(compiledEmail, (sendError: any, body: any) => {
+				if (sendError) {
+					console.log(sendError);
+					return;
+				}
+				return response.json(status);
+			});
+		})
 
 	} catch (error) {
 		const status : Status = {
