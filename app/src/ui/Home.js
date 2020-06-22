37f2bd28b5cb46171ca5a4e6ea3d048ce31d65d7
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-22 17:36:29 -0600

    add home layout

--- app/src/ui/Home.js
+++ app/src/ui/Home.js
@@ -1,9 +1,44 @@
 import React from "react"
+import {Link} from "react-router-dom";
+
+//import {UseJwt} from "../shared/misc/JwtHelpers";
+import {SignInForm} from "signin/SignInForm";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <main className="d-flex align-items-center mh-100">
+        <Container fluid="true">
+          <Row>
+            <Col sm={6} lg={{span: 4, offset: 1}}>
+
+              <SignInForm/>
+
+              {/* only render the signin form if user does not have a jwt, otherwise output a message */}
+              {/*{jwt === null ? (*/}
+              {/*  <SignInForm/>*/}
+              {/*) : (*/}
+              {/*  <div>*/}
+              {/*    <span className="h2 mr-2">You're logged in!</span>*/}
+              {/*    &nbsp;*/}
+              {/*    <Link to="/posts">*/}
+              {/*      <Button className="btn-sm mb-2" variant="outline-dark">Head to Posts&nbsp;&nbsp;*/}
+              {/*        <FontAwesomeIcon icon="arrow-right" />*/}
+              {/*      </Button>*/}
+              {/*    </Link>*/}
+              {/*  </div>*/}
+              {/*)}*/}
+
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </>
   )
 }
