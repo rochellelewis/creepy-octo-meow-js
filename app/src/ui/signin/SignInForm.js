e327b9f2357781bbb21df40d0bbae80b9d07e2a9
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-22 17:37:06 -0600

    add sign in form components

---
+++ app/src/ui/signin/SignInForm.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import {Redirect} from "react-router";
+import {useDispatch} from 'react-redux'
+import * as Yup from "yup";
+import {Formik} from "formik";
+
+import {httpConfig} from "../../utils/http-config";
+
+// this is the form itself - separated into it's own component for simplicity
+import {SignInFormContent} from "./SignInFormContent";
+
+export const SignInForm = () => {
+
+  // state variable to handle redirect to posts page on sign in
+  //const [toPosts, setToPosts] = useState(null);
+
+  const dispatch = useDispatch()
+
+  // form validation
+  const validator = Yup.object().shape({
+    signinEmail: Yup.string()
+      .email("email must be a valid email")
+      .required('email is required'),
+    signinPassword: Yup.string()
+      .required("Password is required")
+  });
+
+  // state variable to hold form data
+  const signIn = {
+    signinEmail: "",
+    signinPassword: ""
+  };
+
+  // sign in form POST submitter
+  const submitSignIn = (values, {resetForm, setStatus}) => {
+    httpConfig.post("/apis/signin/", values)
+      .then(reply => {
+        let {message, type} = reply;
+        if(reply.status === 200 && reply.headers["x-jwt-token"]) {
+          window.localStorage.removeItem("jwt-token");
+          window.localStorage.setItem("jwt-token", reply.headers["x-jwt-token"]);
+          resetForm();
+          setTimeout(() => {
+            // setToPosts(true);
+            window.location = "/posts";
+          }, 750);
+        }
+        setStatus({message, type});
+      });
+  };
+
+  return (
+    <>
+      {/* redirect user to posts page on sign in */}
+      {/*{toPosts ? <Redirect to="/posts" /> : null}*/}
+
+      <Formik
+        initialValues={signIn}
+        onSubmit={submitSignIn}
+        validationSchema={validator}
+      >
+        {SignInFormContent}
+      </Formik>
+    </>
+  )
+};
---
+++ app/src/ui/signin/SignInFormContent.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+import {FormDebugger} from "../../FormDebugger";
+
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/es/FormControl";
+import Button from "react-bootstrap/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+export const SignInFormContent = (props) => {
+
+  const {
+    status,
+    values,
+    errors,
+    touched,
+    dirty,
+    isSubmitting,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    handleReset,
+  } = props;
+
+  return (
+    <>
+      <Card bg="transparent" className="border-0 rounded-0">
+        <Card.Body>
+          <Form onSubmit={handleSubmit}>
+
+            <Form.Group>
+              <Form.Label className="sr-only">Email</Form.Label>
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>
+                    <FontAwesomeIcon icon="envelope"/>
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  id="signinEmail"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  placeholder="Email"
+                  type="email"
+                  value={values.signinEmail}
+                />
+              </InputGroup>
+              {
+                errors.signinEmail && touched.signinEmail && (
+                  <div className="alert alert-danger">
+                    {errors.signinEmail}
+                  </div>
+                )
+              }
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label className="sr-only">Password</Form.Label>
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>
+                    <FontAwesomeIcon icon="key"/>
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  id="signinPassword"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  type="password"
+                  placeholder="Password"
+                  value={values.signinPassword}
+                />
+              </InputGroup>
+              {
+                errors.signinPassword && touched.signinPassword && (
+                  <div className="alert alert-danger">
+                    {errors.signinPassword}
+                  </div>
+                )
+              }
+            </Form.Group>
+
+            <Form.Group className="text-md-right">
+              <Button variant="primary" type="submit">
+                <FontAwesomeIcon icon="sign-in-alt"/>&nbsp;Sign In
+              </Button>
+            </Form.Group>
+
+            {/*for testing purposes only*/}
+            {/*<FormDebugger {...props}/>*/}
+
+          </Form>
+
+          <div className="my-2">
+            <span className="font-weight-light font-italic">Don't have an account?&nbsp;</span>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+
+        </Card.Body>
+      </Card>
+
+      {console.log(status)}
+      {status && (<div className={status.type}>{status.message}</div>)}
+    </>
+  );
+};
