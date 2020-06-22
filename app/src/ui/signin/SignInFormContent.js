39ba1456b3257d48872babd14a2e8cffe02cde68
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-22 17:47:59 -0600

    add form debugger

--- app/src/ui/signin/SignInFormContent.js
+++ app/src/ui/signin/SignInFormContent.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-import {FormDebugger} from "../../FormDebugger";
+import {FormDebugger} from "../shared/components/FormDebugger";
 
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/es/FormControl";
+import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
