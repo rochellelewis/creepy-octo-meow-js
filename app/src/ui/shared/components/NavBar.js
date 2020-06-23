057238351c4d15dea68bb8912e7c6905030a15d1
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-23 00:24:43 -0600

    add signout

--- app/src/ui/shared/components/NavBar.js
+++ app/src/ui/shared/components/NavBar.js
@@ -3,7 +3,7 @@ import {Redirect} from "react-router-dom";
 import {httpConfig} from "../../../utils/http-config";
 import {Link} from "react-router-dom";
 
-//import {UseJwt, UseJwtProfileId, UseJwtUsername} from "../misc/JwtHelpers";
+import {UseJwt, UseJwtProfileId, UseJwtUsername} from "../../../utils/jwt-helpers";
 
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -12,6 +12,27 @@ import Button from "react-bootstrap/Button";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const NavBar = () => {
+
+  const [token, setToken] = useState(null);
+
+  // use jwt-helpers to grab the jwt and username for logged in users
+  const jwt = UseJwt();
+  const username = UseJwtUsername();
+  const profileId = UseJwtProfileId();
+
+  const signOut = () => {
+    httpConfig.get("/apis/signout/")
+      .then(reply => {
+        let {message, type} = reply;
+        if(reply.status === 200) {
+          window.localStorage.removeItem("jwt");
+          console.log(reply);
+          window.location = "/";
+          // setToHome(true);
+        }
+      });
+  };
+
   return (
     <>
       <header>
@@ -19,28 +40,28 @@ export const NavBar = () => {
           <Link to="/">
             <Navbar.Brand>=^ Octo Meow 8.0 ^=</Navbar.Brand>
           </Link>
-          <Navbar.Text className="small font-italic d-none d-md-inline-block">A DDC React Demo.</Navbar.Text>
+          <Navbar.Text className="small font-italic d-none d-md-inline-block">A DDC React + Express Demo.</Navbar.Text>
           <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
 
           <Navbar.Collapse>
             <Nav className="ml-auto">
 
-              {/* conditional render if user has jwt / is logged in */}
-              {/*{jwt !== null && (*/}
-              {/*  <NavDropdown className="nav-link navbar-username" title={"Welcome, " + username + "!"}>*/}
-              {/*    <div className="dropdown-item">*/}
-              {/*      <Link to={`/profile/${profileId}`} className="nav-link">*/}
-              {/*        <FontAwesomeIcon icon="user" />&nbsp;&nbsp;My Profile*/}
-              {/*      </Link>*/}
-              {/*    </div>*/}
-              {/*    <div className="dropdown-divider"></div>*/}
-              {/*    <div className="dropdown-item sign-out-dropdown">*/}
-              {/*      <button className="btn btn-outline-dark" onClick={signOut}>*/}
-              {/*        Sign Out&nbsp;&nbsp;<FontAwesomeIcon icon="sign-out-alt" />*/}
-              {/*      </button>*/}
-              {/*    </div>*/}
-              {/*  </NavDropdown>*/}
-              {/*)}*/}
+              {/* conditional render the sign in form only if user is not logged in */}
+              {jwt !== null && (
+                <NavDropdown className="nav-link navbar-username" title={"Welcome, " + username + "!"} id="navbar-dropdown">
+                  <div className="dropdown-item">
+                    <Link to={`/profile/${profileId}`} className="nav-link">
+                      <FontAwesomeIcon icon="user" />&nbsp;&nbsp;My Profile
+                    </Link>
+                  </div>
+                  <div className="dropdown-divider"></div>
+                  <div className="dropdown-item sign-out-dropdown">
+                    <button className="btn btn-outline-dark" onClick={signOut}>
+                      Sign Out&nbsp;&nbsp;<FontAwesomeIcon icon="sign-out-alt" />
+                    </button>
+                  </div>
+                </NavDropdown>
+              )}
 
               <Link className="nav-link" to="/posts">
                 <Button variant="outline-dark" className="btn-block">
