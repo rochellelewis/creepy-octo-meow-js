dc4131b7e29ae07964f7182a728830ab96adc3bd
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-23 02:37:02 -0600

    add SignUp.js, SignUpForm.js, SignUpFormContent.js

---
+++ app/src/ui/signup/SignUpForm.js
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+import {httpConfig} from "../../utils/http-config";
+import * as Yup from "yup";
+import {Formik} from "formik";
+
+import {SignUpFormContent} from "./SignUpFormContent";
+
+export const SignUpForm = () => {
+
+	const [status, setStatus] = useState(null);
+
+	const signUp = {
+		signupEmail: "",
+		signupUsername: "",
+		signupPassword: "",
+		signupConfirmPassword: ""
+	};
+
+	const validator = Yup.object().shape({
+		signupEmail: Yup.string()
+			.email("Please provide a valid email")
+			.required("Email is required"),
+		signupUsername: Yup.string()
+			.required("Username is required")
+			.min(2, "Username must be at least 2 characters"),
+		signupPassword: Yup.string()
+			.required("Password is required")
+			.min(6, "Password must be at least 6 characters"),
+		signupConfirmPassword: Yup.string()
+			.required("Confirm your password")
+	});
+
+	const submitSignUp = (values, {resetForm, setStatus}) => {
+		httpConfig.post("/apis/signup/", values)
+			.then(reply => {
+				let {message, type} = reply;
+				setStatus({message, type});
+				if(reply.status === 200) {
+					resetForm();
+					setStatus({message, type});
+				}
+			});
+	};
+
+	return (
+		<>
+			<Formik
+				initialValues={signUp}
+				onSubmit={submitSignUp}
+				validationSchema={validator}
+			>
+				{SignUpFormContent}
+			</Formik>
+		</>
+	)
+
+};
---
+++ app/src/ui/signup/SignUpFormContent.js
@@ -0,0 +1,145 @@
+import React from "react";
+
+import {FormDebugger} from "../../ui/shared/components/FormDebugger";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+export const SignUpFormContent = (props) => {
+
+	const {
+		status,
+		values,
+		errors,
+		touched,
+		dirty,
+		isSubmitting,
+		handleChange,
+		handleBlur,
+		handleSubmit,
+		handleReset
+	} = props;
+
+	return (
+		<>
+			<Form onSubmit={handleSubmit}>
+
+				<Form.Group>
+					<InputGroup>
+						<InputGroup.Prepend>
+							<InputGroup.Text>
+								<FontAwesomeIcon icon="user"/>
+							</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							id="signupUsername"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							placeholder="Pick a User Name"
+							type="text"
+							value={values.signupUsername}
+						/>
+					</InputGroup>
+					{
+						errors.signupUsername && touched.signupUsername && (
+							<div className="alert alert-danger">
+								{errors.signupUsername}
+							</div>
+						)
+					}
+				</Form.Group>
+
+				<Form.Group>
+					<InputGroup>
+						<InputGroup.Prepend>
+							<InputGroup.Text>
+								<FontAwesomeIcon icon="envelope"/>
+							</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							id="signupEmail"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							placeholder="Your Email"
+							type="email"
+							value={values.signupEmail}
+						/>
+					</InputGroup>
+					{
+						errors.signupEmail && touched.signupEmail && (
+							<div className="alert alert-danger">
+								{errors.signupEmail}
+							</div>
+						)
+					}
+				</Form.Group>
+
+				<Form.Group>
+					<InputGroup>
+						<InputGroup.Prepend>
+							<InputGroup.Text>
+								<FontAwesomeIcon icon="key"/>
+							</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							id="signupPassword"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							placeholder="Password"
+							type="password"
+							value={values.signupPassword}
+						/>
+					</InputGroup>
+					{
+						errors.signupPassword && touched.signupPassword && (
+							<div className="alert alert-danger">
+								{errors.signupPassword}
+							</div>
+						)
+					}
+				</Form.Group>
+
+				<Form.Group>
+					<InputGroup>
+						<InputGroup.Prepend>
+							<InputGroup.Text>
+								<FontAwesomeIcon icon="ellipsis-h"/>
+							</InputGroup.Text>
+						</InputGroup.Prepend>
+						<FormControl
+							id="signupConfirmPassword"
+							onChange={handleChange}
+							onBlur={handleBlur}
+							placeholder="Confirm Password"
+							type="password"
+							value={values.signupConfirmPassword}
+						/>
+					</InputGroup>
+					{
+						errors.signupConfirmPassword && touched.signupConfirmPassword && (
+							<div className="alert alert-danger">
+								{errors.signupConfirmPassword}
+							</div>
+						)
+					}
+				</Form.Group>
+
+				<Form.Group className="text-md-right">
+					<Button variant="primary" type="submit">
+						<FontAwesomeIcon icon="paw"/>&nbsp;Join Us!
+					</Button>
+				</Form.Group>
+
+				{/*for testing purposes only*/}
+				{/*<FormDebugger {...props}/>*/}
+
+			</Form>
+
+			{console.log(status)}
+			{status && (<div className={status.type}>{status.message}</div>)}
+		</>
+	)
+};
