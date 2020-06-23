e40c3142e7ca92e3522da4813aa18500d212085d
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-23 04:58:46 -0600

    add conditional render to home signin form

--- app/src/ui/Home.js
+++ app/src/ui/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {Link} from "react-router-dom";
 
-//import {UseJwt} from "../shared/misc/JwtHelpers";
+import {UseJwt} from "../utils/jwt-helpers";
 import {SignInForm} from './signin/SignInForm'
 
 import Container from "react-bootstrap/Container";
@@ -11,6 +11,10 @@ import Button from "react-bootstrap/Button";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Home = () => {
+
+  const jwt = UseJwt();
+  console.log(jwt)
+
   return (
     <>
       <main className="d-flex align-items-center mh-100">
@@ -18,22 +22,20 @@ export const Home = () => {
           <Row>
             <Col sm={6} lg={{span: 4, offset: 1}}>
 
-              <SignInForm/>
-
               {/* only render the signin form if user does not have a jwt, otherwise output a message */}
-              {/*{jwt === null ? (*/}
-              {/*  <SignInForm/>*/}
-              {/*) : (*/}
-              {/*  <div>*/}
-              {/*    <span className="h2 mr-2">You're logged in!</span>*/}
-              {/*    &nbsp;*/}
-              {/*    <Link to="/posts">*/}
-              {/*      <Button className="btn-sm mb-2" variant="outline-dark">Head to Posts&nbsp;&nbsp;*/}
-              {/*        <FontAwesomeIcon icon="arrow-right" />*/}
-              {/*      </Button>*/}
-              {/*    </Link>*/}
-              {/*  </div>*/}
-              {/*)}*/}
+              {jwt === null ? (
+                <SignInForm/>
+              ) : (
+                <div>
+                  <span className="h2 mr-2 mb-3 d-inline-block">You're logged in!</span>
+                  &nbsp;
+                  <Link to="/posts">
+                    <Button className="btn-sm mb-2" variant="outline-dark">Head to Posts&nbsp;&nbsp;
+                      <FontAwesomeIcon icon="arrow-right" />
+                    </Button>
+                  </Link>
+                </div>
+              )}
 
             </Col>
           </Row>
