4361c1887861b6ede3335da31c3f33f0fb078b25
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-23 00:11:19 -0600

    add jwt-helpers.js

---
+++ app/src/utils/jwt-helpers.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from "react";
+import * as jwtDecode from "jwt-decode";
+
+/**
+ * Set of custom react hooks to decode profile data from the JSON Web Token
+ *
+ * @author Rochelle Lewis rlewis37@cnm.edu
+ **/
+
+/**
+ * Helper function that grabs the JSON Web Token
+ * out of browser localStorage
+ **/
+export const UseJwt = () => {
+  const [jwt, setJwt] = useState(null);
+
+  useEffect(() => {
+    setJwt(window.localStorage.getItem("jwt"));
+  }, [jwt]);
+
+  return jwt;
+};
+
+/**
+ * Helper function that decodes the profile username from the JSON Web Token
+ **/
+export const UseJwtUsername = () => {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("jwt");
+    if(token !== null) {
+      const decodedJwt = jwtDecode(token);
+      setUsername(decodedJwt.authorization.profileUsername);
+    }
+  }, [username]);
+
+  return username;
+};
+
+/**
+ * Helper function that decodes the profile id from the JSON Web Token
+ **/
+export const UseJwtProfileId = () => {
+  const [profileId, setProfileId] = useState(null);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("jwt");
+    if(token !== null) {
+      const decodedJwt = jwtDecode(token);
+      setProfileId(decodedJwt.authorization.profileId);
+    }
+  }, [profileId]);
+
+  return profileId;
+};
