fc94ca3c344e26433effc9cfe1ab4c4aeeb94f92
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-24 01:26:12 -0600

    add logged in validation to post req

--- apis/src/routes/post.route.ts
+++ apis/src/routes/post.route.ts
@@ -1,9 +1,4 @@
 import {Router} from "express";
-
-import {asyncValidatorController} from "../controllers/asyncValidator.controller";
-import {postValidator} from "../validators/post.validator";
-import {isLoggedIn} from "../validators/isLoggedIn.validator";
-
 import {
 	getAllPostsController,
 	getPostByPostIdController,
@@ -12,13 +7,16 @@ import {
 	deletePostController
 } from "../controllers/post.controller";
 
+import {asyncValidatorController} from "../controllers/asyncValidator.controller";
+import {postValidator} from "../validators/post.validator";
+import {isLoggedIn} from "../controllers/isLoggedIn.controller";
 const {checkSchema} = require("express-validator");
 
 export const PostRoute = Router()
 
 PostRoute.route("/")
 	.get(getAllPostsController)
-	.post(asyncValidatorController(checkSchema(postValidator)), postPostController)
+	.post(isLoggedIn, asyncValidatorController(checkSchema(postValidator)), postPostController)
 
 // todo: this route may not be necessary? Maybe.
 PostRoute.route("/:postId")
