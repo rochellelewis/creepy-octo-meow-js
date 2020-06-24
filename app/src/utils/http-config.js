1bc5e23b19f2d2acea5a7525a1193da329904005
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-06-24 01:23:53 -0600

    update http-config.js boilerplate

--- app/src/utils/http-config.js
+++ app/src/utils/http-config.js
@@ -1,16 +1,12 @@
 import axios from "axios";
 export const httpConfig = axios.create();
 
-httpConfig.interceptors.response.use(function ({data, headers} ) {
-
-  if(data.status === 200) {
-    return (
-      data.data !== null
-        ? {message: null, data: data.data, status: 200, type: " alert alert-success", headers: {...headers}}
-        : {message: data.message, status: 200, type: "alert alert-success", data: null, headers: {...headers}}
-    )
+httpConfig.interceptors.response.use(function ({data, headers}) {
+  if (data.status === 200) {
+    return data.data !== null
+      ? {message: null, data: data.data, status: 200, type: " alert alert-success", headers: {...headers}}
+      : {message: data.message, status: 200, type: " alert alert-success", data: null, headers: {...headers}};
   }
-
   return {message: data.message, status: data.status, type: "alert alert-danger", data: null, headers: {...headers}}
 
 }, function (error) {
@@ -18,3 +14,17 @@ httpConfig.interceptors.response.use(function ({data, headers} ) {
   console.log(error);
   return Promise.reject(error);
 });
+
+httpConfig.interceptors.request.use(
+  (config) => {
+    const authorization = window.localStorage.getItem("authorization")
+    if (authorization) {
+      config.headers['authorization'] = authorization
+    }
+    return config
+  }, (error) => {
+    // Do something with response error
+    console.log(error);
+    return Promise.reject(error);
+  }
+)
