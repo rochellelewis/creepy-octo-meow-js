55196c2a579e541c7e9002f2d85ab049e2915658
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-07-02 04:04:19 -0600

    add edit post form - working :star:

--- app/src/ui/posts/PostEdit.js
+++ app/src/ui/posts/PostEdit.js
@@ -1,4 +1,13 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import {httpConfig} from "../../utils/http-config";
+import * as Yup from "yup";
+import {Formik} from "formik";
+
+import {fetchAllPostsAndProfiles} from '../../store/posts'
+import {PostEditFormContent} from './PostEditFormContent'
+import { UseJwt } from '../../utils/jwt-helpers'
 
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
@@ -9,9 +18,47 @@ export const PostEdit = (props) => {
 	// this is the post passed in via props
 	const {post} = props;
 
+	// controls show/hide modal window
 	const [show, setShow] = useState(false);
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
+
+	const [status, setStatus] = useState(null);
+	const dispatch = useDispatch()
+
+	// grab json web token
+	const jwt = UseJwt();
+
+	const updatedPost = {
+		postTitle: "",
+		postContent: ""
+	};
+
+	const validator = Yup.object().shape({
+		postTitle: Yup.string()
+			.required("A title is required.")
+			.max(64, "No titles longer than 64 characters."),
+		postContent: Yup.string()
+			.required("U gonna post something?")
+			.max(2000, "2000 characters max per meow.")
+	});
+
+	const updatePost = (values, {setStatus}) => {
+		const headers = {'authorization': jwt};
+		httpConfig.put(`/apis/post/${post.postId}`, values, {
+			headers: headers})
+			.then(reply => {
+				let {message, type} = reply;
+				setStatus({message, type});
+				if(reply.status === 200) {
+					dispatch(fetchAllPostsAndProfiles())
+					setTimeout(() => {
+						{handleClose()}
+					}, 750);
+				}
+				setStatus({message, type});
+			});
+	};
 
 	return (
 		<>
@@ -25,17 +72,17 @@ export const PostEdit = (props) => {
 					<Modal.Title>Editing Post #{post.postId}</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<h5>{post.postTitle}</h5>
-					<p>{post.postContent}</p>
+
+					{/* EDIT POST FORM */}
+					<Formik
+						initialValues={post}
+						onSubmit={updatePost}
+						validationSchema={validator}
+					>
+						{PostEditFormContent}
+					</Formik>
+
 				</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleClose}>
-						Cancel
-					</Button>
-					<Button variant="primary" onClick={handleClose}>
-						Save Changes
-					</Button>
-				</Modal.Footer>
 			</Modal>
 		</>
 	)
