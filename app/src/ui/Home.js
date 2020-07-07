f93bac789312b43c9ec5498975b783ac75f3263d
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-07-07 16:46:07 -0600

    fix flexbox layout on Home

--- app/src/ui/Home.js
+++ app/src/ui/Home.js
@@ -1,8 +1,11 @@
 import React from "react"
 import {Link} from "react-router-dom";
 
-import {UseJwt} from "../utils/jwt-helpers";
 import {SignInForm} from './signin/SignInForm'
+import { NavBar } from './shared/components/NavBar'
+import { Footer } from './shared/components/Footer'
+
+import {UseJwt} from "../utils/jwt-helpers";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -12,34 +15,45 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Home = () => {
 
+  // grab json web token
   const jwt = UseJwt();
-  console.log(jwt)
 
   return (
     <>
-      <main className="d-flex align-items-center mh-100">
-        <Container fluid>
-          <Row>
-            <Col sm={6} lg={{span: 4, offset: 1}}>
+      <main className="mh-100 d-flex flex-column">
 
-              {/* only render the signin form if user does not have a jwt, otherwise output a message */}
-              {jwt === null ? (
-                <SignInForm/>
-              ) : (
-                <div>
-                  <span className="h2 mr-2 mb-3 d-inline-block">You're logged in!</span>
-                  &nbsp;
-                  <Link to="/posts">
-                    <Button className="btn-sm mb-2" variant="outline-dark">Head to Posts&nbsp;&nbsp;
-                      <FontAwesomeIcon icon="arrow-right" />
-                    </Button>
-                  </Link>
-                </div>
-              )}
+        <header>
+          <NavBar/>
+        </header>
 
-            </Col>
-          </Row>
-        </Container>
+        <section className="d-flex align-items-center flex-grow-1">
+          <Container fluid>
+            <Row>
+              <Col sm={6} lg={{span: 4, offset: 1}}>
+
+                {/* only render the signin form if user does not have a jwt, otherwise output a message for logged in users */}
+                {jwt === null ? (
+                  <SignInForm/>
+                ) : (
+                  <div>
+                    <span className="h2 mr-2 mb-3 d-inline-block">You're logged in!</span>
+                    &nbsp;
+                    <Link to="/posts">
+                      <Button className="btn-sm mb-2" variant="outline-dark">Head to Posts&nbsp;&nbsp;
+                        <FontAwesomeIcon icon="arrow-right" />
+                      </Button>
+                    </Link>
+                  </div>
+                )}
+
+              </Col>
+            </Row>
+          </Container>
+        </section>
+
+        <section className="text-center text-md-right">
+          <Footer/>
+        </section>
       </main>
     </>
   )
