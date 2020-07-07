dd404c9471eb44dae15f009e1d24a1930f0abcc7
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-07-07 17:36:50 -0600

    fix Profile.js flex layout

--- app/src/ui/Profile.js
+++ app/src/ui/Profile.js
@@ -1,8 +1,11 @@
 import React, {useEffect} from "react";
 import {useSelector, useDispatch} from "react-redux";
-import {UseJwtProfileId} from "../utils/jwt-helpers";
 
+import {UseJwtProfileId} from "../utils/jwt-helpers";
 import {fetchProfileByProfileId} from '../store/profiles'
+
+import { NavBar } from './shared/components/NavBar'
+import { Footer } from './shared/components/Footer'
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -27,30 +30,42 @@ export const Profile = ({match}) => {
 
   return (
     <>
-      <main className="mh-100 d-flex align-items-center">
-        <Container fluid className="py-5">
-          <Row>
-            <Col md="5">
-              <Card className="bg-transparent-90">
-                <Card.Header>
-                  <h2 className="my-0">Hello, {profile && profile.profileUsername}!</h2>
-                </Card.Header>
-                <Card.Body>
-                  <div><span className="font-weight-bold">Username</span>: {profile && profile.profileUsername}</div>
+      <main className="mh-100 d-flex flex-column">
 
-                  {/* only show the private profile data if logged into the same account! */}
-                  {(profile && profile.profileId === currentProfileId) && (
-                    <>
-                      <div><span className="font-weight-bold">Your Profile Id</span>: {profile && profile.profileId}</div>
-                      <div><span className="font-weight-bold">Your Email Address</span>: {profile && profile.profileEmail}</div>
-                    </>
-                  )}
+        <header>
+          <NavBar/>
+        </header>
 
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+        <section className="d-flex align-items-center flex-grow-1">
+          <Container fluid className="py-5">
+            <Row>
+              <Col md="5">
+                <Card className="bg-transparent-90">
+                  <Card.Header>
+                    <h2 className="my-0">Hello, {profile && profile.profileUsername}!</h2>
+                  </Card.Header>
+                  <Card.Body>
+                    <div><span className="font-weight-bold">Username</span>: {profile && profile.profileUsername}</div>
+
+                    {/* only show the private profile data if logged into the same account! */}
+                    {(profile && profile.profileId === currentProfileId) && (
+                      <>
+                        <div><span className="font-weight-bold">Your Profile Id</span>: {profile && profile.profileId}</div>
+                        <div><span className="font-weight-bold">Your Email Address</span>: {profile && profile.profileEmail}</div>
+                      </>
+                    )}
+
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+
+        <section className="text-center text-md-right">
+          <Footer/>
+        </section>
+
       </main>
     </>
   )
