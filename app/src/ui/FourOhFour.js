bf9692e617883bb78b8903d629f2a9491de3a1f0
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-07-07 17:45:14 -0600

    fix FourOhFour.js flex layout

--- app/src/ui/FourOhFour.js
+++ app/src/ui/FourOhFour.js
@@ -1,5 +1,8 @@
 import React from "react"
 
+import { Footer } from './shared/components/Footer'
+import { NavBar } from './shared/components/NavBar'
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -7,14 +10,26 @@ import Col from "react-bootstrap/Col";
 export const FourOhFour = () => {
   return (
     <>
-      <main className="mh-100 d-flex align-items-center">
-        <Container fluid="true">
-          <Row>
-            <Col>
-              <h1>404 Error: Y U NO FIND?</h1>
-            </Col>
-          </Row>
-        </Container>
+      <main className="mh-100 d-flex flex-column">
+
+        <header>
+          <NavBar/>
+        </header>
+
+        <section className="d-flex align-items-center flex-grow-1">
+          <Container fluid>
+            <Row>
+              <Col>
+                <h1>404 Error: Y U NO FIND?</h1>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+
+        <section className="text-center text-md-right">
+          <Footer/>
+        </section>
+
       </main>
     </>
   )
