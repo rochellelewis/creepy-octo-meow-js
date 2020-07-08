5cd119f6806b31750703ec0d98fb3eb0002c014a
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-07-08 01:14:07 -0600

    checkpoint commit - edit profile

--- app/src/ui/profile/ProfileEdit.js
+++ app/src/ui/profile/ProfileEdit.js
@@ -1,25 +1,21 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-
 import {httpConfig} from "../../utils/http-config";
 import * as Yup from "yup";
 import {Formik} from "formik";
 
-
-// rlewis's special helpers :D
 import { UseJwt } from '../../utils/jwt-helpers'
-import { DecodeCharacters } from '../../utils/decode-characters'
+import { fetchProfileByProfileId } from '../../store/profiles'
 
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { fetchAllPostsAndProfiles } from '../../store/posts'
+import { ProfileEditFormContent } from './ProfileEditFormContent'
 
 export const ProfileEdit = (props) => {
 
   // this is the profile passed in via props
   const {profile} = props;
-  console.log(profile.profileId)
 
   // controls show/hide modal window
   const [show, setShow] = useState(false);
@@ -33,28 +29,31 @@ export const ProfileEdit = (props) => {
   const jwt = UseJwt();
 
   /*
-	* Set initial values to the existing profile data, and de-encode
-	* special characters here first for proper output to front end.
-	* Post content will be re-encoded and sanitized on update with
-	* express-validator in the api.
+	* Set initial values to the existing profile data
 	* */
   const updatedProfileContent = {
     profileActivationToken: profile.profileActivationToken,
     profileEmail: profile.profileEmail,
-    profilePassword: null,
+    profilePassword: "",
     profileUsername: profile.profileUsername
   };
 
   const validator = Yup.object().shape({
-    postTitle: Yup.string()
-      .required("A title is required.")
-      .max(64, "No titles longer than 64 characters."),
-    postContent: Yup.string()
-      .required("U gonna post something?")
-      .max(2000, "2000 characters max per meow.")
+    profileEmail: Yup.string()
+      .email("Please provide a valid email")
+      .required("Email is required"),
+    profileUsername: Yup.string()
+      .required("Username is required")
+      .min(2, "Username must be at least 2 characters"),
+    profilePassword: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters"),
+    profilePasswordConfirm: Yup.string()
+      .required("Confirm your password")
+      .min(8, "Password must be at least 8 characters"),
   });
 
-  const updateProfile = (values, {setStatus}) => {
+  const updateProfile = (values, {resetForm, setStatus}) => {
     const headers = {'authorization': jwt};
     httpConfig.put(`/apis/profile/${profile.profileId}`, values, {
       headers: headers})
@@ -62,7 +61,8 @@ export const ProfileEdit = (props) => {
         let {message, type} = reply;
         setStatus({message, type});
         if(reply.status === 200) {
-          dispatch(fetchAllPostsAndProfiles())
+          resetForm();
+          dispatch(fetchProfileByProfileId(profile.profileId))
           setTimeout(() => {
             {handleClose()}
           }, 750);
@@ -80,19 +80,18 @@ export const ProfileEdit = (props) => {
       {/* Modal Window / Edit Post Form */}
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Editing Profile #</Modal.Title>
+          <Modal.Title>Editing Profile #{profile.profileId}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
 
           {/* EDIT PROFILE FORM */}
-          foo!
-          {/*<Formik*/}
-          {/*  initialValues={updatedPostContent}*/}
-          {/*  onSubmit={updatePost}*/}
-          {/*  validationSchema={validator}*/}
-          {/*>*/}
-          {/*  {PostEditFormContent}*/}
-          {/*</Formik>*/}
+          <Formik
+            initialValues={updatedProfileContent}
+            onSubmit={updateProfile}
+            validationSchema={validator}
+          >
+            {ProfileEditFormContent}
+          </Formik>
 
         </Modal.Body>
       </Modal>
---
+++ app/src/ui/profile/ProfileEditFormContent.js
@@ -0,0 +1,147 @@
+import React from "react";
+
+import {FormDebugger} from "../shared/components/FormDebugger";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export const ProfileEditFormContent = (props) => {
+
+  const {
+    submitStatus,
+    status,
+    values,
+    errors,
+    touched,
+    dirty,
+    isSubmitting,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    handleReset
+  } = props;
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+
+        <Form.Group>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                <FontAwesomeIcon icon="user"/>
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              id="profileUsername"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="New User Name"
+              type="text"
+              value={values.profileUsername}
+            />
+          </InputGroup>
+          {
+            errors.profileUsername && touched.profileUsername && (
+              <div className="alert alert-danger">
+                {errors.profileUsername}
+              </div>
+            )
+          }
+        </Form.Group>
+
+        <Form.Group>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                <FontAwesomeIcon icon="envelope"/>
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              id="profileEmail"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="New Email"
+              type="email"
+              value={values.profileEmail}
+            />
+          </InputGroup>
+          {
+            errors.profileEmail && touched.profileEmail && (
+              <div className="alert alert-danger">
+                {errors.profileEmail}
+              </div>
+            )
+          }
+        </Form.Group>
+
+        <Form.Group>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                <FontAwesomeIcon icon="key"/>
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              id="profilePassword"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Enter New Password"
+              type="password"
+              value={values.profilePassword}
+            />
+          </InputGroup>
+          {
+            errors.profilePassword && touched.profilePassword && (
+              <div className="alert alert-danger">
+                {errors.profilePassword}
+              </div>
+            )
+          }
+        </Form.Group>
+
+        <Form.Group>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                <FontAwesomeIcon icon="ellipsis-h"/>
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              id="profilePasswordConfirm"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Confirm New Password"
+              type="password"
+              value={values.profilePasswordConfirm}
+            />
+          </InputGroup>
+          {
+            errors.profilePasswordConfirm && touched.profilePasswordConfirm && (
+              <div className="alert alert-danger">
+                {errors.profilePasswordConfirm}
+              </div>
+            )
+          }
+        </Form.Group>
+
+        <Form.Group>
+          <Button variant="primary" type="submit" className="mr-2">
+            <FontAwesomeIcon icon="paw"/>&nbsp;Update!
+          </Button>
+          <Button variant="outline-dark" type="reset" onClick={handleReset}>Reset</Button>
+        </Form.Group>
+      </Form>
+
+      {/*{console.log(status)}*/}
+      {status && (<div className={status.type}>{status.message}</div>)}
+
+      {/*for testing purposes only*/}
+      {/*<FormDebugger {...props}/>*/}
+    </>
+  )
+};
