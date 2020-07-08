8802fa6d1dea5e23127df6677ea1ee75e9f8bc91
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-07-08 00:36:48 -0600

    cleanup Profile.js

--- app/src/ui/profile/Profile.js
+++ app/src/ui/profile/Profile.js
@@ -6,22 +6,16 @@ import {fetchProfileByProfileId} from '../../store/profiles'
 
 import { NavBar } from '../shared/components/NavBar'
 import { Footer } from '../shared/components/Footer'
+import { ProfileEdit } from './ProfileEdit'
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import { ProfileEdit } from './ProfileEdit'
 
 export const Profile = ({match}) => {
 
   const dispatch = useDispatch();
-
-  // grab the profile id from the JWT for the currently logged in account
-  const currentProfileId = UseJwtProfileId();
-
-  // Return the profile from the redux store
-  const profile = useSelector(state => (state.profiles ? state.profiles[0] : []));
 
   const effects = () => {
     dispatch(fetchProfileByProfileId(match.params.profileId));
@@ -29,6 +23,12 @@ export const Profile = ({match}) => {
 
   const inputs = [match.params.profileId];
   useEffect(effects, inputs);
+
+  // Return the profile from the redux store
+  const profile = useSelector(state => (state.profiles ? state.profiles[0] : null));
+
+  // grab the profile id from the JWT for the logged in user
+  const currentProfileId = UseJwtProfileId();
 
   return (
     <>
@@ -53,7 +53,7 @@ export const Profile = ({match}) => {
                   <Card.Body>
                     <div><span className="font-weight-bold">Username</span>: {profile && profile.profileUsername}</div>
 
-                    {/* only show the private profile data if logged into the same account! */}
+                    {/* only show the private profile data if the user's jwt profileId matches */}
                     {(profile && profile.profileId === currentProfileId) && (
                       <>
                         <div><span className="font-weight-bold">Your Profile Id</span>: {profile && profile.profileId}</div>
