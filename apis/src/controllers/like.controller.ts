461881d6eb4c1da75597c195e74f2335248b30af
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-07-18 20:47:42 -0600

    verify profileId in like.controller.ts on post, delete

--- apis/src/controllers/like.controller.ts
+++ apis/src/controllers/like.controller.ts
@@ -8,6 +8,7 @@ import {deleteLike} from "../../utils/like/deleteLike";
 import {selectLikeByLikePostIdAndLikeProfileId} from "../../utils/like/selectLikeByLikePostIdAndLikeProfileId";
 import {selectLikesByLikePostId} from "../../utils/like/selectLikesByLikePostId";
 import {selectAllLikes} from "../../utils/like/selectAllLikes";
+import {Profile} from "../../utils/interfaces/profile";
 
 
 /**
@@ -20,11 +21,24 @@ import {selectAllLikes} from "../../utils/like/selectAllLikes";
 export async function postLikeController(request: Request, response: Response, nextFunction: NextFunction) {
 	try {
 
+		// grab profile data off of session for authorization
+		const profile: Profile = request.session?.profile
+		const sessionProfileId = <string> profile.profileId
+
 		// grab the like data off the request body
 		const {
 			likePostId,
 			likeProfileId
 		} = request.body;
+
+		// verify likeProfileId matches sessionProfileId - a user can only post likes for their own account!
+		if(sessionProfileId !== likeProfileId) {
+			return response.json({
+				status: 403, // forbidden!
+				data: null,
+				message: "You're not allowed to like meows for this account."
+			});
+		}
 
 		// create the Like to be inserted
 		const like: Like = {
@@ -36,7 +50,7 @@ export async function postLikeController(request: Request, response: Response, n
 		return response.json({status: 200, data: null, message: result})
 
 	} catch(error) {
-		console.log(error)
+		return response.json({status: error.status, data: error.data, message: error.message})
 	}
 }
 
@@ -50,17 +64,30 @@ export async function postLikeController(request: Request, response: Response, n
 export async function deleteLikeController(request: Request, response: Response, nextFunction: NextFunction) {
 	try {
 
-		// grab the like data off the request parameters
+		// grab profile data off of session for authorization
+		const profile: Profile = request.session?.profile
+		const sessionProfileId = <string> profile.profileId
+
+		// grab the like data off the request body
 		const {
 			likePostId,
 			likeProfileId
 		} = request.body;
 
+		// verify likeProfileId matches sessionProfileId - a user can only delete  likes for their own account!
+		if(sessionProfileId !== likeProfileId) {
+			return response.json({
+				status: 403, // forbidden!
+				data: null,
+				message: "You're not allowed to unlike meows for this account."
+			});
+		}
+
 		const result = await deleteLike(likePostId, likeProfileId)
 		return response.json({status: 200, data: null, message: result})
 
 	} catch(error) {
-		console.log(error)
+		return response.json({status: error.status, data: error.data, message: error.message})
 	}
 }
 
