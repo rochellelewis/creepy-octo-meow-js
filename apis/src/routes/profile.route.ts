86f5321b22a592e68e0b5d168e21b0b04ccd81e1
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-09-01 20:26:16 -0600

    remove unnecessary route from profile.route.ts

--- apis/src/routes/profile.route.ts
+++ apis/src/routes/profile.route.ts
@@ -8,7 +8,6 @@ import {
 	getProfileByProfileIdController,
 	putProfileController,
 	getAllProfilesController,
-	getProfileByProfileEmailController,
 	deleteProfileController
 } from "../controllers/profile.controller";
 
@@ -23,7 +22,3 @@ ProfileRoute.route("/:profileId")
 	.get(getProfileByProfileIdController)
 	.put(isLoggedIn, asyncValidatorController(checkSchema(profileValidator)), putProfileController)
 	.delete(isLoggedIn, asyncValidatorController(checkSchema(profileIdValidator)), deleteProfileController)
-
-// todo: correct this path, check controller and get method
-ProfileRoute.route("/profileEmail")
-	.post(getProfileByProfileEmailController)
